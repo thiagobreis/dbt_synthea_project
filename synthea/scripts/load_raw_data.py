467f646aadcb415aa981eaaf19fede7184724f57
Thiago ADM <thiagobreis@gmail.com>
2026-08-04 21:15:40 -0300

    :wrench: configs

--- synthea/scripts/load_raw_data.py
+++ synthea/scripts/load_raw_data.py
@@ -1,18 +1,27 @@
 import duckdb
+from pathlib import Path
 
-con = duckdb.connect('./dev.duckdb')
-con.execute("CREATE SCHEMA IF NOT EXSISTS raw")
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+REPO_ROOT = PROJECT_ROOT.parent
+
+DB_PATH = PROJECT_ROOT / "dev.duckdb"
+CSV_DIR = REPO_ROOT / "csv"
+
+con = duckdb.connect(str(DB_PATH))
+con.execute("CREATE SCHEMA IF NOT EXISTS raw")
+
 
 tables = [
-    'allergies','careplans','claims','claims_transactions','conditions','devices'
-    'encounters','imaging_studies','immnunizations','medications','observations','organizations'
-    'patients','payers_transictions','payers','procedures','providers','supplies'
+    'allergies','careplans','claims','claims_transactions','conditions','devices',
+    'encounters','imaging_studies','immunizations','medications','observations','organizations',
+    'patients','payer_transitions','payers','procedures','providers','supplies'
 ]
 
 for t in tables:
+    print(f"CREATING TABLE {t}")
     con.execute(f"""
        CREATE OR REPLACE TABLE raw.{t} AS
-       SELECT * FROM read_csv_auto('./csv/{t}.csv'), header = True        
+       SELECT * FROM read_csv_auto('../../csv/{t}.csv', header = True)
     """)
     
 con.close()
